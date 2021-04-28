a9c161aab4a98f74d0f4c68459940e308e678892
chung <qc160@naver.com>
2021-04-28 17:04:13 +0900

    hook

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, {useCallback, useState, useRef} from 'react';
 import TodoList from './components/TodoList';
 import TodoInsert from './components/TodoInsert';
 import TodoTemplate from './components/TodoTemplate';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id:1,
+      text: '안녕하세요',
+      checked: true,
+    },
+    {
+      id:2,
+      text: '반갑습니다',
+      checked: true,
+    },
+    {
+      id:3,
+      text: '하이~ 에이치아이',
+      checked: false,
+    },
+  ])
+
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    text => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
+  ); //todos가 바뀌었을 때만 함수 생성
+
   return (
   <TodoTemplate>
-    <TodoInsert/>
-    <TodoList/>
+    <TodoInsert onInsert={onInsert}/>
+    <TodoList todos={todos}/>
   </TodoTemplate>
   )
 }
--- todo-app/src/components/TodoInsert.jsx
+++ todo-app/src/components/TodoInsert.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 import { MdAdd } from 'react-icons/md'
 import './TodoInsert.scss'
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+    const [value, setValue] = useState('');
+    const onChange = useCallback(e => {
+        setValue(e.target.value);
+        console.log(e.target.value);
+    }, []); //컴포넌트가 처음 렌더링될 때만 함수 생성
+
+    //onClick으로도 가능!!
+    const onSubmit = useCallback(
+        e => {
+            onInsert(value);
+            setValue(''); //value 값 초기화
+            e.preventDefault(); //submit 이벤트는 브라우저에서 새로고침을 발생시키는데, 이를 방지하기 위해 이 함수를 호출
+        },
+        [onInsert, value],
+    );
+
     return (
-        <form className="TodoInsert">
-            <input placeholder="할 일을 입력하세요" />
+        <form className="TodoInsert" onSubmit={onSubmit}>
+            <input 
+                placeholder="할 일을 입력하세요" 
+                value={value}
+                onChange={onChange}
+            />
             <button type="submit">
                 <MdAdd />
             </button>
