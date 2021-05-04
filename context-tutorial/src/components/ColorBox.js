8c64b1bf8ca0be388147bfb2a032d81ad52c1ce6
chung <qc160@naver.com>
2021-05-04 17:14:51 +0900

    context complete

--- context-tutorial/src/components/ColorBox.js
+++ context-tutorial/src/components/ColorBox.js
@@ -1,20 +1,56 @@
-import React from 'react';
-import ColorContext from '../contexts/color';
+import React from "react";
+import { ColorConsumer } from "../contexts/color";
 
 const ColorBox = () => {
-    return (
-        <ColorContext.Consumer>
-            {value => (
-                <div
-                    style={{
-                        width: '64px',
-                        height: '64px',
-                        background: value.color
-                    }}                
-                />
-            )}
-        </ColorContext.Consumer>
-    );
+  return (
+    <ColorConsumer>
+      {({ state }) => (
+        <>
+          <div
+            style={{
+              width: "64px",
+              height: "64px",
+              background: state.color,
+            }}
+          />
+          <div
+            style={{
+              width: "32px",
+              height: "32px",
+              background: state.subcolor,
+            }}
+          />
+        </>
+      )}
+    </ColorConsumer>
+  );
 };
 
 export default ColorBox;
+
+// import React, { useContext } from "react";
+// import { ColorContext } from "../contexts/color";
+
+// const ColorBox = () => {
+//   const { state } = useContext(ColorContext);
+//   return (
+//     <>
+//       <div
+//         style={{
+//           width: "64px",
+//           height: "64px",
+//           background: state.color,
+//         }}
+//       />
+//       <div
+//         style={{
+//           width: "32px",
+//           height: "32px",
+//           background: state.subcolor,
+//         }}
+//       />
+//     </>
+//   );
+// };
+
+// export default ColorBox;
